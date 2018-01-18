84958d786a7045a96e03e98ff06695307fff0ab9
Joshua Palacios <palacios.joshua@gmail.com>
2018-01-18 16:14:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ $(document).ready(function (){
 function seachRepositories() {
   const searchInput = document.getElementById("searchTerms").innerHTML;
   const searchTerms = searchInput.split(" ").join("+")
+  debugger
   $get("https://api.github.com/search/repositories?q=" + searchTerms)
 }
