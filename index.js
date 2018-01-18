dbb0b86daa581ed895295f343126e3ec05400d84
Joshua Palacios <palacios.joshua@gmail.com>
2018-01-18 18:08:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function searchRepositories() {
 }
 
 function displayError() {
-  $("#results").html("I'm sorry, there's been an error. Please try again.")
+  $("#errors").html("I'm sorry, there's been an error. Please try again.")
 }
 
 $(document).ready(function (){
