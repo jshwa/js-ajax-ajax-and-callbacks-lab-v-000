717bfd2a43bfd3d221c764314882fceea35fd2d5
Joshua Palacios <palacios.joshua@gmail.com>
2018-01-18 16:43:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
+function seachRepositories() {
+  debugger
+  const searchInput = document.getElementById("searchTerms").value;
+  const searchTerms = searchInput.split(" ").join("+")
+  debugger
+}
+
 $(document).ready(function (){
-  function seachRepositories() {
-    debugger
-    const searchInput = document.getElementById("searchTerms").value;
-    const searchTerms = searchInput.split(" ").join("+")
-    debugger
-  }
 
 });
-
-
