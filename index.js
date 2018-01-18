205de86bddb227203f8512c9d640394557296fff
Joshua Palacios <palacios.joshua@gmail.com>
2018-01-18 16:48:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-function seachRepositories() {
+function getRepositories() {
   console.log("WTF")
-  const searchInput = document.getElementById("searchTerms").value;
-  const searchTerms = searchInput.split(" ").join("+")
+  const username = document.getElementById("username").value;
+  const req = new XMLHttpRequest();
+  req.addEventListener("load", displayRepositories);
+  req.open('GET', `https://api.github.com/users/${username}/repos`);
+  req.send();
 }
 
 $(document).ready(function (){
