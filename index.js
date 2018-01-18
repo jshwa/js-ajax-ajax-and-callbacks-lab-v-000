f2cea8f06f4e920c7f24379199c4776547af49b1
Joshua Palacios <palacios.joshua@gmail.com>
2018-01-18 19:28:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ function displayError() {
   $("#errors").html("I'm sorry, there's been an error. Please try again.")
 }
 
+function displayResults() {
+  function(data){
+    $("#results").html("<ul>" + data.items.map(item => "<li>" + item.name + "- <a href='#' data-url='" + item.url + "/commits" + "'onclick='showCommits(this)'> Get Commits</a></li>") + "</ul>")
+  }
+}
+
 function showCommits(el) {
   $.get(el.dataset.url, function (data) {
     $('#details').html("<ul>"+data.map(commit => "<li>" + commit.commit.author.name + "</li>")+"</ul>")
