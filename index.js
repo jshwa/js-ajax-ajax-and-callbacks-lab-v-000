63df26b085ca05915d6165d8b876a278c2560db1
Joshua Palacios <palacios.joshua@gmail.com>
2018-01-18 16:48:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 function getRepositories() {
   console.log("WTF")
-  const username = document.getElementById("username").value;
-  const req = new XMLHttpRequest();
-  req.addEventListener("load", displayRepositories);
-  req.open('GET', `https://api.github.com/users/${username}/repos`);
-  req.send();
+
 }
 
 $(document).ready(function (){
