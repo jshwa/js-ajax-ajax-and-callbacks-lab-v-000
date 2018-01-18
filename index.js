0dbb3e65f387944adf01f88725c0c1fd40efaad5
Joshua Palacios <palacios.joshua@gmail.com>
2018-01-18 20:45:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function displayResults(data) {
 
 function showCommits(el) {
   $.get(el.dataset.url, function (data) {
-    $('#details').html("<ul>"+data.map(commit => "<li>" + commit.commit.author.name + "</li>")+"</ul>")
+    $('#details').html("<ul>"+ data.map(commit => "<li>" + commit.commit.author.name + "</li>")+"</ul>")
   })
 }
 
