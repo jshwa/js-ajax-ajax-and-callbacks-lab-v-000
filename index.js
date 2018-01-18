5f4b0eef0c886e3dab144c3b1530a0514da47693
Joshua Palacios <palacios.joshua@gmail.com>
2018-01-18 17:52:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,3 @@ $(document).ready(function (){
 });
 
 
-const src = document.getElementById("repository-template").innerHTML
-const template = Handlebars.compile(src)
-const repoList = template(repos)
-document.getElementById("repositories").innerHTML = repoList
