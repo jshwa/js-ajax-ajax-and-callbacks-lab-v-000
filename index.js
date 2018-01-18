7a162bce6689a9bfaf9b2ec24586b99114f4ec4f
Joshua Palacios <palacios.joshua@gmail.com>
2018-01-18 17:34:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 function searchRepositories() {
   const searchTerms = $('#searchTerms').val()
-  $.get("https://api.github.com/search/repositories?q=" + searchTerms, displayResults())
+  $.get("https://api.github.com/search/repositories?q=" + searchTerms, displayResults(response))
 }
 
-function displayResults() {
-  const searchResults = JSON.parse(this.responseText);
+function displayResults(response) {
+  const searchResults = JSON.parse(response);
   const results = $('#results').html();
   results = searchResults.map(r => r.name)
 }
